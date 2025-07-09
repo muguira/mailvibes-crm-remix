053c3c152880ce5ce628b6ed717d6c790bdd78be
Franklin Rodriguez <franklinserif@gmail.com>
2025-07-09 16:46:59 -0400

    chore: import token diagnostics service in main entry file
    - Add import statement for the token diagnostics service to the main application file.
    - This integration is part of ongoing improvements for managing Gmail connection diagnostics.

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { startSyncWorker } from '@/workers/emailSyncWorker';
 import App from './App';
 import './index.css';
+import './services/google/tokenDiagnostics';
 
 // Start the email sync worker
 startSyncWorker({
